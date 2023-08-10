4413bd31a187d5f65a44a743d6e0f3a1161b6b6f
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-10 17:13:45 -0300

    aplicando metodo sacar na classe main

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,7 +1,23 @@
 package com.main;
 
 import com.core.ContaCorrente;
+import com.core.ContaPoupanca;
 
 public class Main {
     public static void main(String[] args) {
+
+        ContaCorrente cc = new ContaCorrente();
+        cc.depositar(500);
+        System.out.println("O saldo inicial da conta corrente: " + cc.getSaldo());
+
+        cc.sacar(150);
+        System.out.println("O saldo atual da conta corrente: " + cc.getSaldo());
+
+        ContaPoupanca cp = new ContaPoupanca();
+        cp.depositar(500);
+        System.out.println("O saldo inicial da conta poupanca: " + cp.getSaldo());
+
+        cp.sacar(150);
+        System.out.println("O saldo atual da conta poupanca: " + cp.getSaldo());
+    }
 }
